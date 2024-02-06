f5922e19dee6d6665bf6e56213ed4a3e29b2722a
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2024-02-06 15:11:23 +0100

    Initial commit

---
+++ src/main/java/alessiaPalmieri/U5W1D2/U5W1D2Application.java
@@ -0,0 +1,13 @@
+package alessiaPalmieri.U5W1D2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W1D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/alessiaPalmieri/U5W1D2/U5W1D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package alessiaPalmieri.U5W1D2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
